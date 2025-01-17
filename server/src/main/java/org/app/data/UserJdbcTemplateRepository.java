a1b356a427a5081a576a042b85e1d2c81fc35b35
angelomrosales <miguelrosalespersonal@gmail.com>
2025-01-17 16:05:57 -0600

    Added findAllUsers()

--- server/src/main/java/org/app/data/UserJdbcTemplateRepository.java
+++ server/src/main/java/org/app/data/UserJdbcTemplateRepository.java
@@ -1,5 +1,6 @@
 package org.app.data;
 
+import org.app.data.mappers.UserMapper;
 import org.app.models.user.User;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -17,7 +18,9 @@ public class UserJdbcTemplateRepository implements UserRepository {
 
     @Override
     public List<User> findAllUsers() {
-        return null;
+
+        final String sql = "SELECT user_id, username, email, `password`, weight, height_ft, height_in;";
+        return jdbcTemplate.query(sql, new UserMapper());
     }
 
     @Override
