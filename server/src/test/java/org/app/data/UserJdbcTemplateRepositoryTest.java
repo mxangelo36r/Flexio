84c715d3f22b3bb2ea3623b695e421916dedcfac
angelomrosales <miguelrosalespersonal@gmail.com>
2025-01-18 17:19:54 -0600

    Reconfigured test files to allow DbTestConfig.java

--- server/src/test/java/org/app/data/UserJdbcTemplateRepositoryTest.java
+++ server/src/test/java/org/app/data/UserJdbcTemplateRepositoryTest.java
@@ -1,23 +1,42 @@
 package org.app.data;
 
+import org.app.models.user.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class UserJdbcTemplateRepositoryTest {
 
-    @Autowired
     UserJdbcTemplateRepository repository;
 
-    @Autowired
-    JdbcTemplate jdbcTemplate;
+    public UserJdbcTemplateRepositoryTest() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(DbTestConfig.class);
+        repository = context.getBean(UserJdbcTemplateRepository.class);
+    }
+
+    @BeforeAll
+    static void oneTimeSetup() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(DbTestConfig.class);
+        JdbcTemplate jdbcTemplate = context.getBean(JdbcTemplate.class);
+        jdbcTemplate.update("call set_known_good_state();");
+    }
 
     @Test
     void findAllUsers() {
+
+        List<User> users = repository.findAllUsers();
+
+        assertNotNull(users);
+        assertEquals(1, users.size());
     }
 
     @Test
