327ae803e8f3b41249ca6f67faf4e7bea6c0d16f
angelomrosales <miguelrosalespersonal@gmail.com>
2025-02-10 16:33:02 -0600

    Refactored

---
+++ server/src/test/java/learn/app/data/UserGoalJdbcTemplateRepositoryTest.java
@@ -0,0 +1,16 @@
+package learn.app.data;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserGoalJdbcTemplateRepositoryTest {
+
+    @Test
+    void addUserGoal() {
+    }
+
+    @Test
+    void updateUserGoal() {
+    }
+}
