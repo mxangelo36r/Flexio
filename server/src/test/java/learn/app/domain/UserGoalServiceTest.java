8c7c5d830007c13685ee532148e3ecd50c3a382a
angelomrosales <miguelrosalespersonal@gmail.com>
2025-02-25 19:07:27 -0600

    Set up validations and created test files: UserGoal

---
+++ server/src/test/java/learn/app/domain/UserGoalServiceTest.java
@@ -0,0 +1,20 @@
+package learn.app.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserGoalServiceTest {
+
+    @Test
+    void findUserGoal() {
+    }
+
+    @Test
+    void addUserGoal() {
+    }
+
+    @Test
+    void updateUserGoal() {
+    }
+}
