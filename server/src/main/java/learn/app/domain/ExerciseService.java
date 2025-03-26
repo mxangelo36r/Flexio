4d9ce4a157c4bbc661efecdecb2fe87cab510f23
mxangelo36r <m.angelo36r@gmail.com>
2025-03-26 18:28:21 -0500

    Added Java validations API in pom and completed Exercise service layer

---
+++ server/src/main/java/learn/app/domain/ExerciseService.java
@@ -0,0 +1,39 @@
+package learn.app.domain;
+
+import learn.app.data.ExerciseRepository;
+import learn.app.models.workout.Exercise;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class ExerciseService {
+
+    private final ExerciseRepository repository;
+
+    public ExerciseService(ExerciseRepository repository) {
+        this.repository = repository;
+    }
+
+    // CRUD Operations (Validated via Spring Boot Starter Validations -> See Exercise in models package)
+
+    public List<Exercise> findAllExercise() {
+        return repository.findAllExercises();
+    }
+
+    public Exercise findExerciseById(int id) {
+        return repository.findExerciseById(id);
+    }
+
+    public Exercise addExercise(Exercise exercise) {
+        return repository.addExercise(exercise);
+    }
+
+    public boolean updateExercise(Exercise exercise) {
+        return repository.updateExercise(exercise);
+    }
+
+    public boolean deleteExercise(int id) {
+        return repository.deleteExercise(id);
+    }
+}
