24197601a28c0f914ec7d3c2822dd718615b8781
mxangelo36r <m.angelo36r@gmail.com>
2025-03-26 17:57:24 -0500

    Set up known good state for exercise test repo

--- server/src/test/java/learn/app/data/ExerciseJdbcTemplateRepositoryTest.java
+++ server/src/test/java/learn/app/data/ExerciseJdbcTemplateRepositoryTest.java
@@ -1,10 +1,31 @@
 package learn.app.data;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class ExerciseJdbcTemplateRepositoryTest {
+
+    @Autowired
+    UserJdbcTemplateRepository repository;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    static boolean hasSetUp = false;
+
+    @BeforeEach
+    void setup() {
+        if (!hasSetUp) {
+            hasSetUp = true;
+            jdbcTemplate.update("call set_known_good_state");
+        }
+    }
 
     @Test
     void findAllExercises() {
