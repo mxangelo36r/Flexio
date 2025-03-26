eb07e96d79fb0a02435265dd5639dc4a84d91f13
mxangelo36r <m.angelo36r@gmail.com>
2025-03-26 17:48:00 -0500

    Added test classes for exercise repository

---
+++ server/src/test/java/learn/app/data/ExerciseJdbcTemplateRepositoryTest.java
@@ -0,0 +1,28 @@
+package learn.app.data;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExerciseJdbcTemplateRepositoryTest {
+
+    @Test
+    void findAllExercises() {
+    }
+
+    @Test
+    void findExerciseById() {
+    }
+
+    @Test
+    void addExercise() {
+    }
+
+    @Test
+    void updateExercise() {
+    }
+
+    @Test
+    void deleteExercise() {
+    }
+}
