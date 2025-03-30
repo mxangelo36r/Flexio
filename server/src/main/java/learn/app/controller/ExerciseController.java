2431cf3cbe428def0845d2b6e677219a83dca8ca
mxangelo36r <m.angelo36r@gmail.com>
2025-03-30 15:47:37 -0500

    Added Error Message handling for invalid parameters and tested unhappy paths

--- server/src/main/java/learn/app/controller/ExerciseController.java
+++ server/src/main/java/learn/app/controller/ExerciseController.java
@@ -5,10 +5,14 @@ import learn.app.domain.result.Result;
 import learn.app.models.workout.Exercise;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("flexio/exercise")
@@ -39,6 +43,22 @@ public class ExerciseController {
     public ResponseEntity<Object> addExercise(@Valid @RequestBody Exercise exercise) {
         Exercise result = service.addExercise(exercise);
         return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+    // Printing out validations
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return errors;
     }
 
 
