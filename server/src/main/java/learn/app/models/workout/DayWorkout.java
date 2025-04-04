b11fdf8bbb87e32707a33a1c7f3360d5d4a505bd
mxangelo36r <m.angelo36r@gmail.com>
2025-04-04 14:47:46 -0500

    Added validations in DayWorkout and created an empty constructor

--- server/src/main/java/learn/app/models/workout/DayWorkout.java
+++ server/src/main/java/learn/app/models/workout/DayWorkout.java
@@ -1,13 +1,24 @@
 package learn.app.models.workout;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public class DayWorkout {
 
     // Fields
     private int dayWorkoutId;
+    @NotNull (message = "Date cannot be empty")
+    @NotBlank (message = "Date cannot be empty")
     private LocalDate date;
+    @NotNull (message = "Workout name cannot be empty")
+    @NotBlank (message = "Workout name cannot be empty")
     private String workoutName;
+
+    // Empty Constructor
+    public DayWorkout() {
+
+    }
 
     // Constructor
     public DayWorkout(int dayWorkoutId, LocalDate date, String workoutName) {
