41962c128f26464464a3bba8c2d562a69c835c28
mxangelo36r <m.angelo36r@gmail.com>
2025-04-04 22:18:40 -0500

    Added persistence dependency

--- server/src/main/java/learn/app/models/workout/Exercise.java
+++ server/src/main/java/learn/app/models/workout/Exercise.java
@@ -7,6 +7,8 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Min;
 
+import javax.persistence.*;
+
 import java.util.Objects;
 
 @UniqueExerciseName
