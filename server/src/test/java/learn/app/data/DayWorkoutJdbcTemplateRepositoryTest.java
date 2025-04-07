f4bb45a1d96ae292da0d97d3143bd285813ce668
mxangelo36r <m.angelo36r@gmail.com>
2025-04-07 15:14:34 -0500

    Created DayWorkout test class

---
+++ server/src/test/java/learn/app/data/DayWorkoutJdbcTemplateRepositoryTest.java
@@ -0,0 +1,28 @@
+package learn.app.data;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DayWorkoutJdbcTemplateRepositoryTest {
+
+    @Test
+    void findAllWorkouts() {
+    }
+
+    @Test
+    void findWorkoutById() {
+    }
+
+    @Test
+    void addWorkout() {
+    }
+
+    @Test
+    void updateWorkout() {
+    }
+
+    @Test
+    void deleteWorkout() {
+    }
+}
