6401d178470e167d9c3b6c03a494944eedde3f65
mxangelo36r <m.angelo36r@gmail.com>
2025-04-06 21:14:20 -0500

    Added updateWorkout()

--- server/src/main/java/learn/app/data/DayWorkoutJdbcTemplateRepository.java
+++ server/src/main/java/learn/app/data/DayWorkoutJdbcTemplateRepository.java
@@ -4,8 +4,12 @@ import learn.app.data.mappers.DayWorkoutMapper;
 import learn.app.models.workout.DayWorkout;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 @Repository
@@ -25,7 +29,7 @@ public class DayWorkoutJdbcTemplateRepository implements DayWorkoutRepository {
 
     @Override
     public DayWorkout findWorkoutById(int id) {
-        final String sql = "SELECT day_workout_id, `day`, `name` FROM day_workout " +
+        final String sql = "SELECT day_workout_id, day, name FROM day_workout " +
                 "WHERE day_workout_id = ?;";
 
         try {
@@ -37,12 +41,37 @@ public class DayWorkoutJdbcTemplateRepository implements DayWorkoutRepository {
 
     @Override
     public DayWorkout addWorkout(DayWorkout dayWorkout) {
-        return null;
+        final String sql = "INSERT INTO day_workout (day, name)" +
+                "VALUES (?, ?);";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        int rowsAffected = jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, dayWorkout.getWorkoutName());
+            ps.setDate(2, java.sql.Date.valueOf(dayWorkout.getDate()));
+            return ps;
+        }, keyHolder);
+
+        if (rowsAffected <= 0) {
+            return null;
+        }
+
+        dayWorkout.setDayWorkoutId(keyHolder.getKey().intValue());
+        return dayWorkout;
     }
 
     @Override
     public boolean updateWorkout(DayWorkout dayWorkout) {
-        return false;
+        final String sql = "UPDATE day_workout SET " +
+                "day = ?, " +
+                "name = ?;";
+
+        int rowsUpdated = jdbcTemplate.update(sql,
+                dayWorkout.getDate(),
+                dayWorkout.getWorkoutName());
+
+        return rowsUpdated > 0;
     }
 
     @Override
