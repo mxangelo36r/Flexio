5dfabb9e0212cb9d2b03207a7bb05f42291e38ab
mxangelo36r <m.angelo36r@gmail.com>
2025-04-06 20:58:42 -0500

    Added DayWorkout repository

---
+++ server/src/main/java/learn/app/data/DayWorkoutRepository.java
@@ -0,0 +1,14 @@
+package learn.app.data;
+
+import learn.app.models.workout.DayWorkout;
+
+import java.util.List;
+
+public interface DayWorkoutRepository {
+
+    List<DayWorkout> findAllWorkouts();
+    DayWorkout findWorkoutById(int id);
+    DayWorkout addWorkout(DayWorkout dayWorkout);
+    boolean updateWorkout(DayWorkout dayWorkout);
+    boolean deleteWorkout(DayWorkout dayWorkout);
+}
