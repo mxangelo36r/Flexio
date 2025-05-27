66e822e6bd36c871ba5ba591a7a0c956688bf420
mxangelo36r <m.angelo36r@gmail.com>
2025-05-27 16:35:06 -0500

    Added back userGoalService class

--- server/src/main/java/learn/app/domain/UserGoalService.java
+++ server/src/main/java/learn/app/domain/UserGoalService.java
@@ -1,29 +1,27 @@
-//package learn.app.domain;
-//
-//import learn.app.data.UserGoalRepository;
-//import learn.app.models.goals.UserGoal;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class UserGoalService {
-//
-//    private final UserGoalRepository repository;
-//
-//    public UserGoalService(UserGoalRepository repository) {
-//        this.repository = repository;
-//    }
-//
-//    // Operations
-//
-//    public UserGoal findUserGoal(int userGoalId) {
-//        return repository.findUserGoal(userGoalId);
-//    }
-//
-//    public UserGoal addUserGoal(UserGoal userGoal) {
-//        return repository.addUserGoal(userGoal);
-//    }
-//
-//    public boolean updateUserGoal(UserGoal userGoal) {
-//        return repository.updateUserGoal(userGoal);
-//    }
-//}
+package learn.app.domain;
+
+import learn.app.data.UserGoalRepository;
+import learn.app.models.goals.UserGoal;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserGoalService {
+
+    private final UserGoalRepository repository;
+
+    public UserGoalService(UserGoalRepository repository) {
+        this.repository = repository;
+    }
+
+    public UserGoal findUserGoal(int userGoalId) {
+        return repository.findUserGoalById(userGoalId);
+    }
+
+    public UserGoal addUserGoal(UserGoal userGoal) {
+        return repository.addUserGoal(userGoal);
+    }
+
+    public boolean updateUserGoal(UserGoal userGoal) {
+        return repository.updateUserGoal(userGoal);
+    }
+}
